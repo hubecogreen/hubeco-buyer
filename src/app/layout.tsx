d388f7e1894421628df1e5723635cb0c79f37829
Niharika <niharikac@mirakitech.com>
2025-07-21 15:43:17 +0530

    change of meta information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,15 +15,18 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Marketplace for Green Building Materials",
+  title: "Buy Eco-Friendly, Low-Carbon Building Materials India Online",
   description:
-    "India's marketplace for green building materials. India's biggest online store for green building materials, bricks, cement, AAC blocks, steel, plumbing, electrical, façade, building envelope, waterproofing, waste water, lighting, flooring, acoustics, dry walls, false ceiling, glazing, façade panels, plaster, construction blocks, construction aggregates, cooling towers, doors & windows, furniture, indoor air quality, insulation, paints & coatings, ready mix concrete, solar photo voltaic module, hybrid thermal panels, oxygen optimizer, STP ready mix plaster, adhesives, WPC doors & Frames and more! Find the largest selection from all brands at the lowest prices in India.",
+    "Discover eco-friendly, low‑carbon building materials online in India. Shop sustainable supplies on Hubeco Marketplace and build smarter, greener projects today.",
   keywords:
-    "Green building materials, sustainable building materials, green construction materials, construction materials, sustainable materials, marketplace for green building materials, online shopping for building materials, online shopping for green building materials, online shopping for sustainable building materials, eco-friendly construction, green building materials, sustainable construction, buy eco materials, B2B construction marketplace, green construction solutions, carbon-neutral materials, environmentally friendly building supplies. bio-digesters, waste management solutions, eco-friendly sanitation, sustainable water treatment, green sewage systems, organic waste recycling, sustainable wastewater management, recycled construction materials. energy-efficient building materials, thermal insulation, solar roofing, smart glass, eco-friendly insulation, cool roof technology, sustainable energy solutions, passive cooling materials. low-carbon cement, green concrete, sustainable construction materials, eco-friendly cement, carbon-neutral concrete, geopolymer concrete, high-performance sustainable cement. sustainable wood, bamboo building materials, reclaimed wood, engineered wood, eco-friendly timber, FSC-certified wood, sustainable forestry materials, wooden green building solutions. recycled building materials, upcycled construction materials, plastic bricks, reclaimed metal, crushed glass aggregates, sustainable raw materials, green building waste solutions.",
+    "Green building materials, sustainable building materials, green construction materials, construction materials, sustainable materials, marketplace for green building materials, online shopping for building materials, online shopping for green building materials, online shopping for sustainable building materials, eco-friendly construction, green building materials, sustainable construction, buy eco materials, B2B construction marketplace, green construction solutions, carbon-neutral materials, environmentally friendly building supplies. bio-digesters, waste management solutions, eco-friendly sanitation, sustainable water treatment, green sewage systems, organic waste recycling, sustainable wastewater management, recycled construction materials. energy-efficient building materials, thermal insulation, solar roofing, smart glass, eco-friendly insulation, cool roof technology, sustainable energy solutions, passive cooling materials. low-carbon cement, green concrete, sustainable construction materials, eco-friendly cement, carbon-neutral concrete, geopolymer concrete, high-performance sustainable cement. sustainable wood, bamboo building materials, reclaimed wood, engineered wood, eco-friendly timber, FSC-certified wood, sustainable forestry materials, wooden green building solutions. recycled building materials, upcycled construction materials, plastic bricks, reclaimed metal, crushed glass aggregates, sustainable raw materials, green building waste solutions",
+  icons: {
+    icon: "/favicon.ico",
+  },
   openGraph: {
-    title: "Marketplace for Green Building Materials",
+    title: "Buy Eco-Friendly, Low-Carbon Building Materials India Online",
     description:
-      "Marketplace for green building materials. India's biggest online store for green building materials, bricks, cement, steel, plumbing, electrical, façade, building envelope, waterproofing, waste water, lighting, flooring, acoustics, dry walls, false ceiling, glazing, façade panels, plaster, construction blocks, construction aggregates, cooling towers, doors & windows, furniture, indoor air quality, insulation, paints& coatings, ready mix concrete, solar photo voltaic module,hybrid thermal panels,STP, oxygen optimizers, AAC, ready mix plaster, adhesives, WPC doors & Frames and more! Find the largest selection from all brands at the lowest prices in India.",
+    "Discover eco-friendly, low‑carbon building materials online in India. Shop sustainable supplies on Hubeco Marketplace and build smarter, greener projects today.",
     siteName: "Hubeco",
     images: [
       {
@@ -33,6 +36,7 @@ export const metadata: Metadata = {
     ],
   },
 };
+
 
 export default function RootLayout({
   children,

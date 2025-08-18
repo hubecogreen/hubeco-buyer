081ad5e8fa4005ce36916a2e92c151572792b24a
Niharika <niharikac@mirakitech.com>
2025-08-18 18:45:16 +0530

    changes

--- src/components/header/SidebarMenu.tsx
+++ src/components/header/SidebarMenu.tsx
@@ -168,6 +168,7 @@ const SidebarMenu: React.FC<SidebarMenuProps> = ({ isOpen, onClose }) => {
 
   const toggleMenu = (id: string) => {
     setOpenMenus((prev) => ({ ...prev, [id]: !prev[id] }));
+    
   };
 
   const router = useRouter();
@@ -264,9 +265,15 @@ const SidebarMenu: React.FC<SidebarMenuProps> = ({ isOpen, onClose }) => {
                 </span>
                 {(hasSubCategories || hasChildCategories) &&
                   (openMenus[category._id] ? (
-                    <FaAngleUp onClick={() => toggleMenu(category._id)} />
+                    <FaAngleUp onClick={() => {
+                      toggleMenu(category._id);
+                      setSeoCategorySlug(category.seoSlug || "");
+                    }} />
                   ) : (
-                    <FaAngleDown onClick={() => toggleMenu(category._id)} />
+                    <FaAngleDown onClick={() => {
+                      toggleMenu(category._id);
+                      setSeoCategorySlug(category.seoSlug || "");
+                    }} />
                   ))}
               </div>
               {(hasSubCategories || hasChildCategories) && (
@@ -274,6 +281,7 @@ const SidebarMenu: React.FC<SidebarMenuProps> = ({ isOpen, onClose }) => {
                   className={`${styles.submenu} ${
                     openMenus[category._id] ? styles.open : ""
                   } pl-4 flex flex-col gap-2`}
+                  
                 >
                   {/* Show subCategories if they exist */}
                   {hasSubCategories &&
@@ -374,7 +382,6 @@ const SidebarMenu: React.FC<SidebarMenuProps> = ({ isOpen, onClose }) => {
         isOpen ? styles.open : ""
       } w-[400%] bg-[url('/images/home/sidebarBg.webp')] bg-cover bg-bottom bg-no-repeat !z-[99999]`}
     >
-      <div className="absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-black/10 to-transparent rounded-r-[48px]" />
       <div className="flex justify-end p-4 w-full md:w-11/12">
         <AiOutlineClose
           size={28}

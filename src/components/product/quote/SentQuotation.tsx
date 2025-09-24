c1ec9a4fc2d4206f89f7618e80ef3b7672770662
Niharika <niharikac@mirakitech.com>
2025-09-24 19:26:00 +0530

    Quote details

--- src/components/product/quote/SentQuotation.tsx
+++ src/components/product/quote/SentQuotation.tsx
@@ -218,7 +218,7 @@ export default function SentQuotation({
                         // window.open(`${assetURL}/${order?.buyerInvoiceLink}`, "_blank")
                         handleDownload(
                           `${assetURL}/${initialState?.quoteDetailsInvoice?.pdfUrl}`,
-                          `Hubeco_Quote_${initialState?.quoteId}`
+                          `Hubeco_${initialState?.quoteId}`
                         )
                       }
                       variant={"outline"}

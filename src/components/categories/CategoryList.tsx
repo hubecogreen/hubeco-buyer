4040fcde06e7aa29ab5684dcc057206de9e0175e
Sandeep-SPAPLC <sandeep.dasari@spaesl.com>
2026-01-23 17:42:23 +0530

    text of categy name size is cahnged for mobile

--- src/components/categories/CategoryList.tsx
+++ src/components/categories/CategoryList.tsx
@@ -292,7 +292,7 @@ const MobileRowCarousel = ({ items }: { items: any[] }) => {
           />
 
           <p
-            className={`${styles.cattitle} text-[13px] md:text-[18px] lg:text-[18px] font-medium  text-primary group-hover:text-white md:mt-4 mt-2 text-center `}
+            className={`${styles.cattitle} text-[9px] md:text-[18px] lg:text-[18px] font-medium  text-primary group-hover:text-white md:mt-4 mt-1 text-center `}
           >
             {category?.name}
           </p>

2af27cd991db19c7d5f94bcd12844d381a63af85
ayaz-mohd <ayaz.mohammed@spaesl.com>
2026-03-04 16:16:36 +0530

    Added watsapp logo

--- src/components/WhatsApp.tsx
+++ src/components/WhatsApp.tsx
@@ -15,10 +15,10 @@ const WhatsAppWidget = () => {
       style={{ pointerEvents: 'auto' }}
     >
       <Image
-        src="/images/whatsapp-1.png" // Place a WhatsApp icon image in your public folder as whatsapp-icon.webp "D:\hubeco\hubeco-buyer\public\images\whatsapp (1).png"
+        src="/images/whatsapp-1.svg" // Place a WhatsApp icon image in your public folder as whatsapp-icon.webp "D:\hubeco\hubeco-buyer\public\images\whatsapp (1).png"
         alt="Chat on WhatsApp"
-        width={50}
-        height={50}
+        width={150}
+        height={150}
       />
     </a>
   );
